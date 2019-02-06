78f5271d64448df9002c5f612cc935fde153a0e6
vvdhout <valentijnvdh@gmail.com>
2019-02-06 17:23:57 +0100

    Fixing truffle-config gas limit issue

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -49,8 +49,7 @@ module.exports = {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-     gas: 669999
+     network_id: "*"       // Any network (default: none)
     },
 
 
